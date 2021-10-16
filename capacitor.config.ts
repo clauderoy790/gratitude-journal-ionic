3f8ccce7d0c7f122e5d70984a73436b35b925d22
Claude Roy <claude.roy790@gmail.com>
2021-10-16 09:52:32 -0500

    release first release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.roy.dailygratitude',
   appName: 'Daily Gratitude',
   webDir: 'www',
   bundledWebRuntime: false,
